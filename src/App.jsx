690e03d6f6c19fb448730596d0fab42941c9f0f4
franxcor <129801200+franxcor@users.noreply.github.com>
2025-01-17 17:29:35 -0500

    updating wiki page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,89 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import wikiLogo from './../public/imgs/wikiLogo.png'
+import menuLines from './../public/imgs/menuLines.png'
+import portrait from './../public/imgs/franx.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="content">
+      <div id="header">
+        <div id="headerMenu">
+          <img src={menuLines} alt ="menuLines" id="menuLines"></img>
+        </div>
+        <div id="headerLogo">
+          <img src={wikiLogo} alt ="wikipedia Logo" id="wikiLogo"></img>
+        </div>
+        <div id="search">
+          <input type="search" id="searchBar" placeholder="Search Wikipedia"></input>
+          <input type="submit" id="submitButton" value="Search"></input>
+        </div>
+        <div id="links">
+          <a>Donate</a>
+          <a>Create</a>
+          <a>Login</a>
+          <a>Other</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+
+      <div id="body">
+        <div id="contentTable">
+          <h5> Contents </h5>
+          <ul>
+            <li>(Top)</li>
+            <li> Early Life 
+              <ul>
+                <li> 2004 </li>
+              </ul>
+            </li>
+            <li> Student Life 
+              <ul>
+                <li> 2004 </li>
+              </ul>
+            </li>
+            <li> Future Goals
+              <ul>
+                <li> 2004 </li>
+              </ul>
+            </li>
+          </ul>
+
+
+        </div>
+        <div id="mainBody">
+          <h1 id="name"> Francesca Corbishley </h1>
+        </div>
+        <div id="bio">
+          <img id="portrait" src={portrait} alt = "Francesca"></img>
+          <p id="portraitCaption"> Francesca & Friends 2024 </p>
+          <div id="bioInfo">
+            <ul id = "titles">
+              <li> Born <br></br><br></br><br></br> </li>
+              <li> Occupation </li>
+              <li> Alma Matter </li>
+              <li> Email </li>
+            </ul>
+            <ul id = "information">
+              <li> Francesca Xinhui Corbishley <br></br> May 31, 2004 (age 20) <br></br> Pasadena, California, U.S. <br></br> </li>
+              <li> Web Development & Design Student </li>
+              <li> Purdue Univesity </li>
+              <li> fcorbish@purdue.edu </li>
+            </ul>
+
+          </div>
+          
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div id="footer">
+
+      </div>
+    </div>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Assign1', //needs to be the same as the project
 })
