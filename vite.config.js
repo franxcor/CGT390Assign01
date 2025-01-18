276b6801c6ab00b5c146c1a0ef5c0ad36337f81e
franxcor <129801200+franxcor@users.noreply.github.com>
2025-01-17 22:32:00 -0500

    update name again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'CGT390Assign1', //needs to be the same as the project
+  base: '/CGT390Assign1', //needs to be the same as the project
 })
